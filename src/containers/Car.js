663b8f0219cd42d07bcc260eae5215e170266569
g33zy <stevejgaz@gmail.com>
2023-06-07 20:30:20 -0500

    test-user

--- src/containers/Car.js
+++ src/containers/Car.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux'
 // import the visual React component "Home"
-import Home from '../components/Home'
+import Car from '../components/Home'
 
 const mapStateToProps = (state) => {
     return {
@@ -10,4 +10,4 @@ const mapStateToProps = (state) => {
 }
 
 // wrap the visual React Component "Home" with the Redux Container Component Home
-export default connect(mapStateToProps)(Home)
+export default connect(mapStateToProps)(Car)
